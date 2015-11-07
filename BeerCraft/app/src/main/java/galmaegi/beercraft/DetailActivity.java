7647c3907c7db650781e0c43df0379463b36ea52
KyoungMin Kim <rlaudals2374@gmail.com>
2015-11-07 14:36:05 +0900

    volley

--- BeerCraft/app/src/main/java/galmaegi/beercraft/DetailActivity.java
+++ BeerCraft/app/src/main/java/galmaegi/beercraft/DetailActivity.java
@@ -2,11 +2,25 @@ package galmaegi.beercraft;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by kyoungmin on 2015-10-24.
  */
 public class DetailActivity extends BaseActivity {
+    private static String TAG = "DETAILACTIVITY";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +31,46 @@ public class DetailActivity extends BaseActivity {
 
         //공통뷰들을 찾아주기 위한 작업을 수행해야함
         findViewById(R.id.detail_left_side).findViewById(R.id.btn_home).setOnClickListener(this);
+        getDetailjson();
     }
+    private void getDetailjson() {
+
+        final String testURL = "http://beerexchange.dnktechnologies.com/wp-content/plugins/beer-rest-api/lib/class-wp-json-draft.php";
+
+        JsonArrayRequest jsonObjReq = new JsonArrayRequest(testURL,
+                new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    // Parsing json object response
+                    // response will be a json object
+                    JSONObject test = (JSONObject)response.get(0);
+                    String name = test.getString("style");
+                    Toast.makeText(DetailActivity.this, name, Toast.LENGTH_SHORT).show();
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+                // hide the progress dialog
+
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq);
+    }
+
 }

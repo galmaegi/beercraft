fec92811a2c405af669a56c853ed2c1f12d9b7eb
galmaegi <rlaudals2374@gmail.com>
2016-01-01 17:18:49 +0900

    sort init

--- BeerCraft/app/src/main/java/galmaegi/beercraft/Beer/BeerIndexPagerFragment.java
+++ BeerCraft/app/src/main/java/galmaegi/beercraft/Beer/BeerIndexPagerFragment.java
@@ -18,7 +18,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.Locale;
 
 import galmaegi.beercraft.AppController;
 import galmaegi.beercraft.CustomTimer.CustomTimer;
@@ -35,6 +38,9 @@ public class BeerIndexPagerFragment extends Fragment {
     ArrayList<BeerIndexItem> items = null;
 
     CustomTimer timer;
+
+    public static BeerIndexPagerFragment beerIndexDraft;
+    public static BeerIndexPagerFragment beerIndexBottled;
 
     @Override
     public void onPause() {
@@ -57,11 +63,98 @@ public class BeerIndexPagerFragment extends Fragment {
             return false;
         }
     });
+    Handler sortHandler = new Handler(new android.os.Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            sortList(msg.arg1);
+            return false;
+        }
+    });
+
+    static class CharAscCompare implements Comparator<BeerIndexItem> {
+
+        /**
+         * 오름차순(ASC)
+         */
+        public int sortArg;
+        public BeerIndexItem lhs;
+        public BeerIndexItem rhs;
+        public CharAscCompare(int sortArg){
+            this.sortArg = sortArg;
+        }
+//        @Override
+//        public int compare(ArrayList<BeerIndexItem> lhs, ArrayList<BeerIndexItem> rhs) {
+//            return 0;
+//        }
+        @Override
+        public int compare(BeerIndexItem lhs, BeerIndexItem rhs) {
+            int returnValue = -1;
+            if(sortArg == 0){
+                returnValue = sortByName();
+            } else if(sortArg == 1){
+                returnValue = sortByStyle();
+            } else if(sortArg == 2){
+                returnValue = sortByPercent();
+            } else if(sortArg == 3){
+                returnValue = sortByMl();
+            } else if(sortArg == 4){
+                returnValue = sortByChange();
+            }
+            return returnValue;
+        }
+        public int sortByName(){
+            if(GlobalVar.language == Locale.KOREAN) {
+                return lhs.getBeerName().compareTo(rhs.getBeerName());
+            }
+            else {
+                return lhs.getEnglishName().compareTo(rhs.getEnglishName());
+            }
+        }
+        public int sortByStyle(){
+            return lhs.getStyle().compareTo(rhs.getStyle());
+        }
+        public int sortByPercent(){
+            if(lhs.getStrength() - rhs.getStrength() <0)
+                return -1;
+            else if(lhs.getStrength() - rhs.getStrength() > 0)
+                return 1;
+            else
+                return 0;
+        }
+        public int sortByMl(){
+            return (lhs.getVolume() - rhs.getVolume());
+        }
+        public int sortByChange(){
+            return lhs.getIncrease() - rhs.getIncrease();
+        }
+
+    }
+
+    public void sortList(int sortArg){
+        BeerIndexPagerFragment fragment;
+        if(mPage == 0){
+            fragment = beerIndexDraft;
+        } else{
+            fragment = beerIndexBottled;
+        }
+        Collections.sort(fragment.items, new CharAscCompare(sortArg));
+        fragment.beerIndexAdapter.notifyDataSetChanged();
+
+    }
+
+
 
     public static BeerIndexPagerFragment newInstance(int page) {
         Bundle args = new Bundle();
         args.putInt(ARG_PAGE, page);
         BeerIndexPagerFragment fragment = new BeerIndexPagerFragment();
+
+        if(page == 0){
+            beerIndexDraft = fragment;
+        } else{
+            beerIndexBottled = fragment;
+        }
+
         fragment.setArguments(args);
         return fragment;
     }
